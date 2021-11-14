56e00567d29156628c98124c730126e7633994de
Davide Zattra <d.zattra@gmail.com>
2021-11-14 16:21:15 +0100

    adeed and runned faker

--- database/seeds/TravelsTableSeeder.php
+++ database/seeds/TravelsTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use Faker\Generator as Faker;
 use App\Models\Travel;
 use Illuminate\Database\Seeder;
+
 
 class TravelsTableSeeder extends Seeder
 {
@@ -10,38 +12,50 @@ class TravelsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $travels = [
-            [
-                "destination"=> 'destination',
-                "commercial_name"=> 'natale alle maldive',
-                "duration" => 15,
-                "departure_date" => '2021/12/21',
-                "description" => 'Lorem ipsum',
-                "price" => 1555.55,
-            ],
-            [
-                "destination"=> 'destination',
-                "commercial_name"=> 'natale alle canarie',
-                "duration" => 15,
-                "departure_date" => '2021/12/21',
-                "description" => 'Lorem ipsum',
-                "price" => 1555.55,
-            ]
+        // $travels = [
+        //     [
+        //         "destination"=> 'destination',
+        //         "commercial_name"=> 'natale alle maldive',
+        //         "duration" => 15,
+        //         "departure_date" => '2021/12/21',
+        //         "description" => 'Lorem ipsum',
+        //         "price" => 1555.55,
+        //     ],
+        //     [
+        //         "destination"=> 'destination',
+        //         "commercial_name"=> 'natale alle canarie',
+        //         "duration" => 15,
+        //         "departure_date" => '2021/12/21',
+        //         "description" => 'Lorem ipsum',
+        //         "price" => 1555.55,
+        //     ]
 
-        ];
+        // ];
 
-        foreach ( $travels as $travel){
+        // foreach ( $travels as $travel){
+        //     $newTravel = new Travel();
+        //     $newTravel->destination = $travel['destination'];
+        //     $newTravel->commercial_name = $travel['commercial_name'];
+        //     $newTravel->duration = $travel['duration'];
+        //     $newTravel->departure_date = $travel['departure_date'];
+        //     $newTravel->description = $travel['description'];
+        //     $newTravel->price = $travel['price'];
+        //     $newTravel->save();
+
+        // }
+
+        for($i = 0; $i < 50; $i++)
+        {
             $newTravel = new Travel();
-            $newTravel->destination = $travel['destination'];
-            $newTravel->commercial_name = $travel['commercial_name'];
-            $newTravel->duration = $travel['duration'];
-            $newTravel->departure_date = $travel['departure_date'];
-            $newTravel->description = $travel['description'];
-            $newTravel->price = $travel['price'];
+            $newTravel->destination = $faker->city();
+            $newTravel->commercial_name = $faker->catchPhrase(1);
+            $newTravel->duration = $faker->numberBetween(0, 100);
+            $newTravel->departure_date = $faker->date('Y_m_d');
+            $newTravel->description = $faker->text(150);
+            $newTravel->price = $faker->randomFloat(2, 150, 1000);
             $newTravel->save();
-
         }
     }
 }
